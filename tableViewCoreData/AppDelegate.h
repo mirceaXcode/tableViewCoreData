21fd6670f56fe35edf599553b5a747c1b8fef0a0
Mircea Popescu <mirceap@gmail.com>
2018-08-09 11:19:15 -0400

    Initial Commit

---
+++ tableViewCoreData/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  tableViewCoreData
+//
+//  Created by Mircea Popescu on 8/9/18.
+//  Copyright © 2018 Mircea Popescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ tableViewCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableViewCoreData
+//
+//  Created by Mircea Popescu on 8/9/18.
+//  Copyright © 2018 Mircea Popescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
